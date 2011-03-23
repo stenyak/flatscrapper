96c265678edcde6cd9aa5d958cd650fa6efcf973
Bruno Gonzalez <stenyak@stenyak.com>
2011-03-23 19:00:20 +0100

    Fixed possible future bug with unknown flat type (private vs inmobiliaria vs unknown) Added check for dependencies at start Added basic colorful logging framework GetCSV should now be parallelizable to several processes without interlocking to cache file

--- idealistaFlat.sh
+++ idealistaFlat.sh
@@ -26,46 +26,119 @@
 
 function cachePiso
 {
-    piso=$1
-    cache=$2
-    urlBase="http://www.idealista.com/pagina/inmueble?codigoinmueble="
+    local piso=$1
+    local cache=$2
+    local urlBase="http://www.idealista.com/pagina/inmueble?codigoinmueble="
     curl --silent $urlBase$piso | html2text > $cache
 }
-
 function getFrase
 {
-    palabra=$1
-    frase=$2
+    local palabra=$1
+    local frase=$2
     echo $frase | sed "s/.*[\.\,]\([^\.\,]*$palabra[^\.\,]*\)[\.\,].*/\1/g"
 }
-cache="/tmp/idealista.tmp"
+function escape
+{
+    local text="$*"
+    echo $text |sed "s/\,//g;s/\"//g;s/'//g" |sed "s/^\ *\s*//g;s/\ *\s*$//g"
+}
+function setColors
+{
+    export RESET="\033[00m"
+    export GREEN="\033[01;32m"
+    export WHITE="\033[01;37m"
+    export YELLOW="\033[01;33m"
+    export RED="\033[01;31m"
+}
+function checkErr
+{
+    # Checks the error code, and displays the error message and exits if error was found (err != 0)
+    local err=$1
+    local msg=$2
+    local quit=$3
+    setColors
+    if [ "$err" -ne "0" ]; then
+        logerror "$msg"
+        if [ "$quit" == "false" ]; then
+            return $err
+        else
+            exit $err
+        fi
+    fi
+}
+function logerror
+{
+    # Displays the desired error message
+    local msg=$*              # error message to display
+    setColors
+    echo -e " $RED>>>>>$WHITE Error!$RESET $msg"
+}
+function loginfo
+{
+    # Displays the desired info message
+    local msg=$*              # info message to display
+    echo -e " $GREEN>>>>>$RESET $msg"
+}
+function checkDep
+{
+    local cmd=$1
+    local msg="Command '$cmd' is missing. Please install and retry."
+    msg="$msg Use your package manager to install it. E.g.: sudo apt-get install $cmd"
+    which $cmd &>/dev/null
+    checkErr $? "$msg" false
+    return $?
+}
+function checkDeps
+{
+    # Checks all necessary tools are installed
+    local deps="$*"     # list of dependencies to check
+    local error="0"
+    for dep in $deps
+    do
+        checkDep $dep
+        if [ $? -ne 0 ]; then error="1"; fi
+    done
+
+    return $error
+}
+function getCsv
+{
+    local piso=$1
+    local cache="/tmp/idealista.$piso.tmp"
+    cachePiso $piso $cache
+
+    local dormitorios=$(cat $cache |grep "[0-9]\ dormitorios" |head -n 1|sed "s/\ dormitorios//g")
+    local banos=$(cat $cache |grep "[0-9]\ wc$" |sed "s/\ wc//g")
+    local eurmes=$(cat $cache |grep "eur\/mes, " |sed "s/\ eur.*//g")
+    local metros=$(cat $cache |grep "eur\/mes, " |sed "s/.*mes, //g;s/\ .*//g")
+    local planta=$(cat $cache |grep "^\(bajo\|planta\|entreplanta\)" |sed "s/bajo/0/g;s/entreplanta/0.5/g;s/planta //g;s/planta //g;s/[^0-9]*ascensor//g" |grep -v " "|head -n 1)
+    local aire=$((0$(cat $cache |grep "^aire acondicionado" |sed "s/aire.*/1/g")))
+    local ascensor=$((0$(cat $cache |grep "con ascensor" |sed "s/.*con ascensor/1/g" |head -n 1)))
+    local garaje=$(cat $cache |grep "plaza de garaje incluida en el precio$" |sed "s/\ plaza de garaje incluida.*//g")
+    if [ "$garaje" == "" ]; then garaje="$(getFrase "araje" "$(cat $cache |grep -i garaje)")"; fi
+    if [ "$garaje" == "" ]; then garaje=0; fi
+    local garaje=$(escape $garaje)
+    local comision=$(echo $piso |grep "VW" >/dev/null && echo "0" || (cat $cache |grep "sin comisiones" >/dev/null && echo "0" || echo "1"))
+    local amueblado=$(cat $cache |grep amueblado >/dev/null && echo "1" || echo "0")
+    local comunidad=$(cat $cache |grep "comunidad incluida en el alquiler" >/dev/null && echo "0" || (cat $cache |grep " eur al mes de gastos de comunidad$" | sed "s/\ eur al mes de gastos de comunidad//g"))
+    if [ "$comunidad" == "" ]; then comunidad=$(cat $cache |grep -i "comunidad inclu" >/dev/null && echo "0" || getFrase "comunidad" "$(cat $cache |grep -i comunidad)"); fi
+    if [ "$comunidad" == "" ]; then comunidad="n/a"; fi
+    #zona TODO
+    local piscina=$(cat $cache |grep -i "piscina" >/dev/null && echo "1" || echo "0")
+
+    echo "piso=$piso, m2=$metros, eur/mes=$eurmes, comision=$comision, comunidad=$comunidad, planta=$planta, ascensor=$ascensor, dormitorios=$dormitorios, baños=$banos, amueblado=$amueblado, aire=$aire, garaje=$garaje, piscina=$piscina"
+}
 if [ -z $1 ]
 then
     echo "Pon el codigo de piso o pisos"
     exit 0
 fi
 
+checkDeps html2text curl sed grep 
 for i in $*
 do
     piso=$i
-    cachePiso $piso $cache
-    dormitorios=$(cat $cache |grep "[0-9]\ dormitorios" |head -n 1|sed "s/\ dormitorios//g")
-    banos=$(cat $cache |grep "[0-9]\ wc$" |sed "s/\ wc//g")
-    eurmes=$(cat $cache |grep "eur\/mes, " |sed "s/\ eur.*//g")
-    metros=$(cat $cache |grep "eur\/mes, " |sed "s/.*mes, //g;s/\ .*//g")
-    planta=$(cat $cache |grep "^\(bajo\|planta\|entreplanta\)" |sed "s/bajo/0/g;s/entreplanta/0.5/g;s/planta //g;s/planta //g;s/[^0-9]*ascensor//g" |grep -v " "|head -n 1)
-    aire=$((0$(cat $cache |grep "^aire acondicionado" |sed "s/aire.*/1/g")))
-    ascensor=$((0$(cat $cache |grep "con ascensor" |sed "s/.*con ascensor/1/g" |head -n 1)))
-    garaje=$(cat $cache |grep "plaza de garaje incluida en el precio$" |sed "s/\ plaza de garaje incluida.*//g")
-    if [ "$garaje" == "" ]; then garaje="$(getFrase "araje" "$(cat $cache |grep -i garaje)")"; fi
-    if [ "$garaje" == "" ]; then garaje=0; fi
-    comision=$(echo $piso |grep "VP" >/dev/null && (cat $cache |grep "sin comisiones" >/dev/null && echo "0" || echo "1") || echo "0")
-    amueblado=$(cat $cache |grep amueblado >/dev/null && echo "1" || echo "0")
-    comunidad=$(cat $cache |grep "comunidad incluida en el alquiler" >/dev/null && echo "0" || (cat $cache |grep " eur al mes de gastos de comunidad$" | sed "s/\ eur al mes de gastos de comunidad//g"))
-    if [ "$comunidad" == "" ]; then comunidad=$(cat $cache |grep -i "comunidad inclu" >/dev/null && echo "0" || getFrase "comunidad" "$(cat $cache |grep -i comunidad)"); fi
-    if [ "$comunidad" == "" ]; then comunidad="n/a"; fi
-    piscina=$(cat $cache |grep -i "piscina" >/dev/null && echo "1" || echo "0")
-    echo "piso=$piso, m2=$metros, eur/mes=$eurmes, comision=$comision, comunidad=$comunidad, planta=$planta, ascensor=$ascensor, dormitorios=$dormitorios, baños=$banos, amueblado=$amueblado, aire=$aire, garaje=$garaje, piscina=$piscina"
+    getCsv $piso
     #exit 0
 done
 
